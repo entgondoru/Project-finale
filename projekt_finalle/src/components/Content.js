a19a1bb574c6f0eee87e4220595b6ca85f59a14e
Entgondoru <entgondoru@wp.pl>
2018-08-30 11:37:08 +0200

    Simirandomizer wersja końcowa

--- projekt_finalle/src/components/Content.js
+++ projekt_finalle/src/components/Content.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import blank from "../Images/blank.jpg";
 
 class Content extends Component {
     constructor(props) {
@@ -8,6 +9,7 @@ class Content extends Component {
     render() {
         let text = this.props.states.bio;
         text = text.split("<", 1)[0];
+        if(text.length<10){text="Artist have no shortened bio"}
         if (this.props.states.ready) {
             return (
                 <div className="container">
@@ -23,7 +25,7 @@ class Content extends Component {
                                 users did it!</h3>
                             <h3><span className="tag">{this.props.states.tag}</span> this is first tag of your artist
                             </h3>
-                            <img className="image" alt="../Images/blank.jpg" src={this.props.states.img}/>
+                            <img className="image" alt={blank} src={this.props.states.img}/>
                         </div>
                         <div className="col3 col">
                             <p className="bio">{text}</p>
--- projekt_finalle/src/components/Submited.js
+++ projekt_finalle/src/components/Submited.js
@@ -24,23 +24,31 @@ function submited() {
                             this.setState({
                                 block2:true
                             });
-                            return;
+                            return
                         } else {
+                            console.log(data);
+                            console.log(data.similarartists.artist);
                             this.setState({secondtab: data.similarartists.artist,
                                             block2:false})
                         }
                     })
-                    .then(this.setState({
-                        randomnumber: Math.ceil(Math.random() * (18 + 1)),
+                    .then(
+                        this.setState({
+                        randomnumber: Math.ceil(Math.random() * (17 + 1)),
                         coin: Math.ceil(Math.random() * (1 + 1))
-                    }))
+                    })
+                    )
                     .then(() => {
                         console.log(this.state.randomnumber);
+                        if(this.state.firsttab.length===0){alert("artist one is not included in aplication");return;}
+                        if(this.state.secondtab.length===0){alert("artist two is not included in aplication");return;}
                         if (this.state.coin === 1&&!this.state.block1&&!this.state.block2) {
+                            console.log(this.state.firsttab)
+                            console.log(this.state.firsttab[this.state.randomnumber]);
                             fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${this.state.firsttab[this.state.randomnumber].name}&api_key=4867da29d8818eb2ef25cc9c1f43a966&format=json`)
                                 .then((data) => data.json())
                                 .then(data => {
-                                    console.log(data.artist);
+                                    console.log(data);
                                     this.setState({
                                         listeners: data.artist.stats.listeners,
                                         playcount: data.artist.stats.playcount,
@@ -56,11 +64,12 @@ function submited() {
                                     console.log(e)
                                 })
                         } else if(this.state.coin===2&&!this.state.block1&&!this.state.block2) {
-                            console.log(this.state.randomnumber)
-                            console.log(this.state.secondtab[this.state.randomnumber].name)
+                            console.log(this.state.secondtab)
+                            console.log(this.state.secondtab[this.state.randomnumber]);
                             fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${this.state.secondtab[this.state.randomnumber].name}&api_key=4867da29d8818eb2ef25cc9c1f43a966&format=json`)
                                 .then((data) => data.json())
                                 .then(data => {
+                                    console.log(data);
                                     this.setState({
                                         listeners: data.artist.stats.listeners,
                                         playcount: data.artist.stats.playcount,
